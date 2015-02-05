64ab079fb33a659243fa71eb65208583e3ad729f
paulhemmings <phemmings@tacitknowledge.com>
2015-02-05 10:31:09 -0800

    rename consumer jsp page

--- src/main/java/com/razor/springRabbit/controllers/QueueConsumerController.java
+++ src/main/java/com/razor/springRabbit/controllers/QueueConsumerController.java
@@ -13,6 +13,6 @@ public class QueueConsumerController
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		return "hello";
+		return "queueConsumer";
 	}
 }
